d3ebcd66cc9d4c450bd827cb6b3b9cef7bb76388
jbuckn1234 <jnorvi5@wgu.edu>
2025-05-11 21:10:00 -0400

    Add build script to upgrade pip

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+<<<<<<< HEAD
 
 # Upgrade pip
 echo "ðŸ”§ Upgrading pip..."
@@ -7,3 +8,11 @@ pip install --upgrade pip
 # Install Python dependencies
 echo "ðŸ“¦ Installing dependencies from requirements.txt..."
 pip install -r requirements.txt
+=======
+echo "í´§ Upgrading pip..."
+pip install --upgrade pip
+
+echo "í³¦ Installing dependencies..."
+pip install -r requirements.txt
+
+>>>>>>> 0e6306f (Add build script to upgrade pip)
