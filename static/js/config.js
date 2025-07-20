51a4b924e7c206d62b4cee5db8a4616e8d736105
Jerit Norville <jnorvi5@wgu.edu>
2025-07-20 13:02:39 -0400

    Create config.js

---
+++ static/js/config.js
@@ -0,0 +1,7 @@
+// Central configuration file for API endpoints
+const CONFIG = {
+  API_URL: 'https://gorillacamping.azurewebsites.net',
+  STATIC_URL: 'https://gorillacamping.site',
+  ENABLE_CACHING: true,
+  CACHE_TTL: 300000 // 5 minutes
+};
